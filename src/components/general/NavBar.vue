adf3efec141eacda95a886bc771620a7847f5fc9
Wika Silo <wikanyaa@gmail.com>
2019-10-15 21:07:20 +0700

    navbar checkpoin done

--- src/components/general/NavBar.vue
+++ src/components/general/NavBar.vue
@@ -1,8 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav>
+    <div id="logo"><img src="~@/assets/images/newassets/orishine-logo.png" alt="orishine"></div>
+    <div id="navbar">
+      <div id="links">
+        <ul>
+          <li><img src="~@/assets/images/icons/burger.svg" alt="burger"></li>
+          <li><img src="~@/assets/images/flag-en.png" alt="flag"></li>
+          <li><span class="Order">Order</span></li>
+        </ul>
+      </div>
+      <div id="icons">
+        <ul>
+          <li><img src="~@/assets/images/newassets/fb.png" alt="fb"></li>
+          <li><img src="~@/assets/images/newassets/ig.png" alt="ig"></li>
+          <li><button type="button" class="Rectangle btn">Order Now</button></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -11,17 +26,50 @@ export default {
 }
 </script>
 
-<style>
-#nav {
-  padding: 30px;
+<style scoped>
+nav {
+  padding: 60px
 }
 
-#nav a {
+.Order {
+  font-family: GoogleSans;
+  font-size: 25px;
   font-weight: bold;
-  color: #2c3e50;
+  color: #ffffff;
+}
+.Rectangle {
+  width: 220px;
+  height: 40px;
+  border-radius: 5px;
+  background-image: linear-gradient(to left, #ffca65, #f58220);
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#navbar{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+#navbar ul {
+  padding: 0px;
+  margin: 0px;
+}
+
+#navbar ul li{
+  display: inline;
+  text-decoration: none;
+  margin: 0px 20px;
+}
+
+#navbar #links ul li:first-child{
+  margin-left: 0px
+}
+#navbar #icons ul li:last-child{
+  margin-right: 0px
+}
+#logo{
+  position: absolute;
+  top: 68px;
+  margin: 0 auto;
+  width: calc(100% - 120px);
 }
 </style>
